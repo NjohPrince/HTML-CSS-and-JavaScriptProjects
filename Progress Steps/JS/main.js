1cda74820a40f662759299c277149248953b4259
NjohPrince <jufredprince@gmail.com>
2020-12-05 15:34:27 +0100

    grabbed all the elements needing control within the js file assigning them to constants

---
+++ Progress Steps/JS/main.js
@@ -0,0 +1,5 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
