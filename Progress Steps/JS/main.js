c39fe2e8c02057b47eed54950ee209a198219354
NjohPrince <jufredprince@gmail.com>
2020-12-05 15:38:17 +0100

    increased the width of the progress bar using and added the active class to the current circle child node js EventListener

--- Progress Steps/JS/main.js
+++ Progress Steps/JS/main.js
@@ -3,3 +3,13 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
+let currentActive = 0
+let newWidth = 0
+
+next.addEventListener('click', () => {
+    currentActive++;
+    newWidth += 33;
+    progress.style.width = newWidth + "%";
+    circles[currentActive].classList.add('active')
+    
+})
