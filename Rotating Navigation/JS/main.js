54a679042779f6cb35372bea31b9b4ffac59d4cf
NjohPrince <jufredprince@gmail.com>
2020-12-05 21:58:24 +0100

    added on event listener on the hamburger menu such that when clicked enables the rotating of the various elements

--- Rotating Navigation/JS/main.js
+++ Rotating Navigation/JS/main.js
@@ -2,3 +2,11 @@ const open = document.getElementById('open')
 const content = document.getElementById('content')
 const nav = document.getElementById('nav')
 const close = document.getElementById('close')
+
+close.style.display = 'none';
+open.addEventListener('click', () => {
+    content.classList.add('content-rotate');
+    nav.classList.add('nav-rotate');
+    open.style.display = 'none'
+    close.style.display = 'block';
+})
