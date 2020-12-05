382f7c05e195cac9e9799da6aea988a091d2d72b
NjohPrince <jufredprince@gmail.com>
2020-12-05 21:56:30 +0100

    grabbed the elenents that will require some js events into the js file usind getters

---
+++ Rotating Navigation/JS/main.js
@@ -0,0 +1,4 @@
+const open = document.getElementById('open')
+const content = document.getElementById('content')
+const nav = document.getElementById('nav')
+const close = document.getElementById('close')
