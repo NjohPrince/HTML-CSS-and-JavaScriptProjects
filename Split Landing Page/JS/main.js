679c34a746ae71dfee6949bbe2853c5fa67c5314
NjohPrince <jufredprince@gmail.com>
2020-12-06 17:36:54 +0100

    added some transitions to enable smooth increase in width

--- Split Landing Page/JS/main.js
+++ Split Landing Page/JS/main.js
@@ -4,5 +4,6 @@ const container = document.querySelector('.container')
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseout', () => container.classList.remove('hover-left'))
+
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
 right.addEventListener('mouseout', () => container.classList.remove('hover-right'))
