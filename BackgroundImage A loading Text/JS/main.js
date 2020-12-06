2600bdb85889fb7b61da323e884de7f7f8ee50dd
NjohPrince <jufredprince@gmail.com>
2020-12-06 13:27:55 +0100

    added the blurry fade away from the background-image

--- BackgroundImage A loading Text/JS/main.js
+++ BackgroundImage A loading Text/JS/main.js
@@ -11,6 +11,8 @@ function blurring(){
     }
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
+
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
