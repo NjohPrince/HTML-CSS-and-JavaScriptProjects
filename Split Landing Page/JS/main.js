0ea78cad967f823071c0fe3082b3aeae5bcd3266
NjohPrince <jufredprince@gmail.com>
2020-12-06 17:23:17 +0100

    added the hover transitions that is width change between the two different sections dynamically using js

--- Split Landing Page/JS/main.js
+++ Split Landing Page/JS/main.js
@@ -2,3 +2,7 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseout', () => container.classList.remove('hover-left'))
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseout', () => container.classList.remove('hover-right'))
