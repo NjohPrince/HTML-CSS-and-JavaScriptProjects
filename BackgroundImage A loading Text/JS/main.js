7c924bdd456713803fa42e67e3b415a2e6b3162f
NjohPrince <jufredprince@gmail.com>
2020-12-06 13:22:49 +0100

    added the blurry efect ont the loader

--- BackgroundImage A loading Text/JS/main.js
+++ BackgroundImage A loading Text/JS/main.js
@@ -10,4 +10,9 @@ function blurring(){
         clearInterval(int)
     }
     loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
