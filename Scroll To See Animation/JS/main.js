c3e3d68b9aa9b15b22400d75c922550ec60b8c28
NjohPrince <jufredprince@gmail.com>
2020-12-06 15:29:53 +0100

    done with the JS and everything working properly

--- Scroll To See Animation/JS/main.js
+++ Scroll To See Animation/JS/main.js
@@ -2,8 +2,10 @@ const boxes = document.querySelectorAll('.box')
 
 window.addEventListener('scroll', checkBoxes)
 
+checkBoxes()
+
 function checkBoxes(){
-    const triggerBottom = window.innerHeight / 5 + 4
+    const triggerBottom = window.innerHeight / 5 * 4
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
