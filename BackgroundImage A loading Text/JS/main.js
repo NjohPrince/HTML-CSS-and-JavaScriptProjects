5584c5837a65f220c1eca7024723e547e66cded1
NjohPrince <jufredprince@gmail.com>
2020-12-06 12:52:35 +0100

    changed the percentage vaklue loading on the page using JS

--- BackgroundImage A loading Text/JS/main.js
+++ BackgroundImage A loading Text/JS/main.js
@@ -6,5 +6,8 @@ let int = setInterval(blurring, 30)
 
 function blurring(){
     load++;
-    console.log(load)
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
 }
